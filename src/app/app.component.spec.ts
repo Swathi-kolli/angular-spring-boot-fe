247b26c7fceb1bf0e8dd21ea6039a443f62fee69
Swathi.K <kolliswathi502@gmail.com>
2022-02-09 22:47:01 +0530

    angular app with Okta

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,27 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { OKTA_CONFIG, OktaAuthModule } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  const config = {
+    issuer: 'https://not-real.okta.com',
+    clientId: 'fake-client-id',
+    redirectUri: 'http://localhost:4200'
+  };
+  const oktaAuth = new OktaAuth(config);
+
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        OktaAuthModule
       ],
       declarations: [
         AppComponent
       ],
+      providers: [{provide: OKTA_CONFIG, useValue: { oktaAuth }}]
     }).compileComponents();
   }));
 
@@ -29,7 +40,7 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('notes app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('notes app is running!');
   });
 });
